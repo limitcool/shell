44fd39c2dc500480e0d0856ca6b8d162c6e2ae68
limitcool <initcoool@gmail.com>
2022-07-19 19:08:25 +0800

    Update install.sh

--- bash/install.sh
+++ bash/install.sh
@@ -2,6 +2,7 @@ wget -N https://raw.githubusercontents.com/fscarmen/warp/main/menu.sh
 wget https://go.dev/dl/go1.18.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
+echo "export PATH=$PATH:/usr/local/go/bin" >>  ~/.bashrc
 apt install gcc -y
 # apt install vim -y
 apt install tmux -y
