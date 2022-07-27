3c626581f72fb4be03b6634101f9ded47fa034bb
limitcool <initcoool@gmail.com>
2022-07-27 13:03:52 +0800

    Add powershell.ps1

--- bash/install.sh
+++ bash/install.sh
@@ -1,8 +1,9 @@
-wget -N https://raw.githubusercontents.com/fscarmen/warp/main/menu.sh 
-wget https://go.dev/dl/go1.18.4.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.4.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-echo "export PATH=$PATH:/usr/local/go/bin" >>  ~/.bashrc
+# wget -N https://raw.githubusercontents.com/fscarmen/warp/main/menu.sh 
+# wget https://go.dev/dl/go1.18.4.linux-amd64.tar.gz
+# rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.4.linux-amd64.tar.gz
+# export PATH=$PATH:/usr/local/go/bin
+# echo "export PATH=$PATH:/usr/local/go/bin" >>  ~/.bashrc
+apt install wget -y
 apt install gcc -y
 # apt install vim -y
 apt install tmux -y
@@ -14,7 +15,7 @@ pip3 install yt-dlp
 wget https://raw.githubusercontent.com/limitcool/shell/master/bash/nohup.sh
 chmod u+x nohup.sh
 
-wget https://github.com/fatedier/frp/releases/download/v0.44.0/frp_0.44.0_linux_amd64.tar.gz
-tar -xvf frp_0.44.0_linux_amd64.tar.gz
+# wget https://github.com/fatedier/frp/releases/download/v0.44.0/frp_0.44.0_linux_amd64.tar.gz
+# tar -xvf frp_0.44.0_linux_amd64.tar.gz
 
 # cd frp_0.44.0_linux_amd64
---
+++ powershell/powershell.ps1
@@ -0,0 +1,3 @@
+[environment]::SetEnvironmentvariable("PATH", "$([environment]::GetEnvironmentvariable("Path", "User"));C:\Program Files\WindowsApps\TheDebianProject.DebianGNULinux_1.12.2.0_x64__76v4gfsz19hv4", "User")
+[environment]::SetEnvironmentvariable("PATH", "$([environment]::GetEnvironmentvariable("Path", "User"));C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.13.11432.0_x64__8wekyb3d8bbwe", "User")
+[environment]::SetEnvironmentvariable("PATH", "$([environment]::GetEnvironmentvariable("Path", "User"));C:\Users\Andorid\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0\LocalCache\local-packages\Python39\Scripts\pip.exe", "User")
